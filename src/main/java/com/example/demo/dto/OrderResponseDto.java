353f1f2ad65230193c7e687d202c2921f89fc3e4
hayeon <hayaon0603@naver.com>
2026-02-04 17:34:36 +0900

    feat: add order request and response DTO

---
+++ src/main/java/com/example/demo/dto/OrderResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Order;
+import lombok.Getter;
+
+@Getter
+public class OrderResponseDto {
+    private Long orderId;
+    private Long productId;
+    private String productName;
+
+    public OrderResponseDto(Order order) {
+        this.orderId = order.getId();
+        this.productId = order.getProduct().getId();
+        this.productName = order.getProduct().getName();
+    }
+}
